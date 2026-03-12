901c14e0a8bcc9b585c4e18f0d5223b83c8d5519
wadafacc <schlumpftimo14@gmail.com>
2026-03-12 08:23:17 +0100

    fix: move to toplevel

--- src/scrapegoat/src/lib.rs
+++ src/scrapegoat/src/lib.rs
@@ -1,8 +1,64 @@
 mod scraper;
 
+use std::io;
+
+use crate::scraper::{pool::ClientPool, proxy::Proxies, useragent::UserAgents};
+
+pub struct ScrapeGoat {
+  user_agents: UserAgents,
+  pool: ClientPool, // client list
+}
+
+pub struct Error {
+  pub status: u16,
+  pub msg: String,
+}
+impl Error {
+  pub fn new(status: u16, msg: String) -> Self {
+    Self { status, msg }
+  }
+}
+
+impl ScrapeGoat {
+  pub fn new(
+    proxy_file: &str,
+    useragents_file: &str,
+    max_concurrent: usize,
+  ) -> Result<Self, io::Error> {
+    let proxies = Proxies::new(proxy_file)?;
+    let user_agents = UserAgents::new(useragents_file)?;
+
+    Ok(Self {
+      user_agents,
+      pool: ClientPool::new(proxies, max_concurrent),
+    })
+  }
+
+  pub async fn get_page(&mut self, url: &str) -> Result<String, Error> {
+    // fetch client or error if no permit
+    let Ok(client) = self.pool.get() else {
+      return Err(Error::new(500, "no permit".to_string()));
+    };
+
+    // get page / throw err?
+    let res = match client
+      .get(url)
+      .header("User-Agent", self.user_agents.get_agent())
+      .send()
+      .await
+    {
+      Ok(r) => Ok(r.text().await.expect("text not texting?")),
+      Err(e) => Err(Error::new(e.status().unwrap().as_u16(), e.to_string())),
+    };
+
+    _ = self.pool.drop(); // return permit
+
+    res
+  }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::scraper::{ScrapeGoat, Scraper};
 
   #[tokio::test]
   async fn get_google() {
